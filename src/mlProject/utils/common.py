f87d6e8787c9ec49dc7c1fc8fec6d2552320c44f
gregorywmorris <gwm1980@gmail.com>
2024-06-17 13:23:26 -0400

    corrected import order for src/mlProject/utils/common.py

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -1,13 +1,13 @@
 import os
-from box.exceptions import BoxValueError
-import yaml
-from mlProject import logger
 import json
-import joblib
-from ensure import ensure_annotations
-from box import ConfigBox
 from pathlib import Path
 from typing import Any
+import joblib
+from ensure import ensure_annotations
+import yaml
+from box import ConfigBox
+from box.exceptions import BoxValueError
+from mlProject import logger
 
 
 
